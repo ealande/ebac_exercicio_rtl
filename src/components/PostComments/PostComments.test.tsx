80ab814fa2ed841b5604e8dcaec3845108a18972
ealande <erickalan23@gmail.com>
2023-08-29 13:33:14 -0300

    teste de inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,27 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+    test('Deve renderizar o componente corretamente', () => {
+        const { debug } = render(<PostComment/>);
+        debug()
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+
+    test('Deve adicionar dois comentários na publicação', () => {
+        render(<Post />)
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target:{
+                value: 'comentario1'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target:{
+                value: 'comentario2'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('comentario1') && screen.getByText('comentario2')).toBeInTheDocument()
+    })
+}); 
